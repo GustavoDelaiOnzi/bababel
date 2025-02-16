dec64fa65517b8a6038e200305b07df3d1209ad4
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-16 00:14:05 -0300

    feat: add consume method

--- bababel/worker/worker.py
+++ bababel/worker/worker.py
@@ -1,10 +1,36 @@
+from typing import List
+
+from bababel.abstracts.client import IClient
+from bababel.abstracts.queue_callback_bind import QueueCallbackBind
 from bababel.rabbitmq.client import RabbitMQClient
 
 
 class Worker:
-    def __init__(self):
-        self.connection = None
-        self.client = RabbitMQClient()
+    def __init__(self,
+                 host: str,
+                 port: int,
+                 username: str,
+                 password: str):
+        self._client: IClient = self._set_client(RabbitMQClient())
+        self._connection = self._connect(host=host, port=port, username=username, password=password)
+        self._channel = None
+        self._queues: List[str] = []
 
-    def connect(self, host: str, port: int, username: str, password: str):
-        self.connection = self.client.connect(host=host, port=port, username=username, password=password)
+    def _connect(self, host: str, port: int, username: str, password: str):
+        return self._client.connect(host=host, port=port, username=username, password=password)
+
+    def consume(self, queue_callback_bind: QueueCallbackBind) -> None:
+        self._ensure_channel()
+        self._channel.queue_declare(queue=queue_callback_bind.queue,
+                                    durable=True)
+        self._channel.basic_consume(queue=queue_callback_bind.queue,
+                                    on_message_callback=queue_callback_bind.callback,
+                                    auto_ack=False)
+        self._channel.start_consuming()
+
+    def _ensure_channel(self) -> None:
+        if not self._channel:
+            self._channel = self._connection.channel()
+
+    def _set_client(self, client: IClient) -> IClient:
+        return client
