86ab89de837e2b2113ea60823e143760d9633df8
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-19 23:05:15 -0300

    docs: add docstrings

--- bababel/rabbitmq/rabbitmq_client.py
+++ bababel/rabbitmq/rabbitmq_client.py
@@ -2,12 +2,26 @@ from pika.connection import ConnectionParameters
 from pika.credentials import PlainCredentials
 
 from bababel.abstracts.client import IClient
-from bababel.abstracts.connection import Connection
 from bababel.rabbitmq.rabbitmq_connection import RabbitMQConnection
 
 
 class RabbitMQClient(IClient):
-    def connect(self, host: str, port: int, username: str, password: str) -> Connection:
+    """
+    Class responsible for communication with RabbitQ broker.
+    """
+    def connect(self, host: str, port: int, username: str, password: str) -> RabbitMQConnection:
+        """
+        Establishes a connection to a RabbitMQ broker.
+
+        Args:
+            host (str): The RabbitMQ server hostname or IP address.
+            port (int): The port number for the RabbitMQ service.
+            username (str): The username for authentication.
+            password (str): The password for authentication.
+
+        Returns:
+            RabbitMQConnection: An instance of RabbitMQConnection with the established connection.
+        """
         credentials = PlainCredentials(username=username, password=password)
 
         params = ConnectionParameters(host=host, port=port, credentials=credentials)
