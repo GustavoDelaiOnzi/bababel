922f20bba8536db94968ce22b0adb755ec211a07
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-20 21:55:26 -0300

    test: add missing tests of RabbitMQConnection

--- tests/rabbitmq/test_rabbitmq_connection.py
+++ tests/rabbitmq/test_rabbitmq_connection.py
@@ -10,6 +10,11 @@ def mock_conn(mocker):
     yield mocker.patch("bababel.rabbitmq.rabbitmq_connection.BlockingConnection")
 
 
+@pytest.fixture(autouse=True)
+def mock_channel(mock_conn):
+    yield mock_conn.return_value.channel
+
+
 class TestRabbitMQConnection:
     def setup_method(self):
         self.params = Mock()
@@ -18,8 +23,39 @@ class TestRabbitMQConnection:
     def sut(self):
         return RabbitMQConnection(parameters=self.params)
 
-    def test_should_init(self, sut, mock_conn):
+    def test_queue_declare(self, sut, mock_channel):
+        # GIVEN
+        args = ['arg1', 'arg2']
+        kwargs = {
+            'kwarg1': 'value1',
+            'kwarg2': 'value2'
+        }
+        # WHEN
+        response = sut.queue_declare(*args, **kwargs)
+
+        # THEN
+        mock_channel.return_value.queue_declare.assert_called_once_with(*args, **kwargs)
+        assert response == mock_channel.return_value.queue_declare.return_value
+
+    def test_basic_consume(self, sut, mock_channel):
+        # GIVEN
+        args = ['arg1', 'arg2']
+        kwargs = {
+            'kwarg1': 'value1',
+            'kwarg2': 'value2'
+        }
+        # WHEN
+        response = sut.basic_consume(*args, **kwargs)
+
+        # THEN
+        mock_channel.return_value.basic_consume.assert_called_once_with(*args, **kwargs)
+        assert response == mock_channel.return_value.basic_consume.return_value
+
+    def test_start_consuming(self, sut, mock_channel):
         # GIVEN
         # WHEN
+        response = sut.start_consuming()
+
         # THEN
-        mock_conn.assert_called_once_with(parameters=self.params)
+        mock_channel.return_value.start_consuming.assert_called_once_with()
+        assert response == mock_channel.return_value.start_consuming.return_value
