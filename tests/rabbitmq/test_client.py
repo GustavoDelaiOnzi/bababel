f2cd7ea8fa45223e01589af3d9ee24ed104874e0
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-20 21:26:52 -0300

    test: fix TestRabbitMQClient

--- tests/rabbitmq/test_client.py
+++ tests/rabbitmq/test_client.py
@@ -29,32 +29,31 @@ class TestRabbitMQClient:
     def sut(self):
         return RabbitMQClient()
 
-    @pytest.fixture(autouse=True)
-    def sut_connect(self, sut):
-        yield sut.connect(host=self.host, port=self.port, username=self.username, password=self.password)
-
-    def test_should_return_connection(self, sut_connect, mock_conn):
+    def test_should_return_connection(self, sut, mock_conn):
         # GIVEN
         # WHEN
+        response = sut.connect(host=self.host, port=self.port, username=self.username, password=self.password)
         # THEN
-        assert sut_connect == mock_conn.return_value
+        assert response == mock_conn.return_value
 
-    def test_should_call_connection_with_correct_params(self, mock_conn, mock_conn_params):
+    def test_should_call_connection_with_correct_params(self, sut, mock_conn, mock_conn_params):
         # GIVEN
         # WHEN
+        sut.connect(host=self.host, port=self.port, username=self.username, password=self.password)
         # THEN
         mock_conn.assert_called_once_with(parameters=mock_conn_params.return_value)
 
-
-    def test_should_call_connection_parameters_with_correct_params(self, mock_credentials):
+    def test_should_call_connection_parameters_with_correct_params(self, sut, mock_credentials):
         # GIVEN
         # WHEN
+        sut.connect(host=self.host, port=self.port, username=self.username, password=self.password)
         # THEN
         mock_credentials.assert_called_once_with(username=self.username, password=self.password)
 
-    def test_should_call_credentials_with_correct_params(self, mock_conn_params, mock_credentials):
+    def test_should_call_credentials_with_correct_params(self, sut, mock_conn_params, mock_credentials):
         # GIVEN
         # WHEN
+        sut.connect(host=self.host, port=self.port, username=self.username, password=self.password)
         # THEN
         mock_conn_params.assert_called_once_with(host=self.host, port=self.port,
                                                  credentials=mock_credentials.return_value)
