0c9fa822c893d40b9ef3a5b1dfbad9864b3dfa6c
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-22 10:26:06 -0300

    feat: create BababelApp object

---
+++ bababel/bababel_app.py
@@ -0,0 +1,10 @@
+import nanoid
+
+
+class BababelApp:
+    def __init__(self, host: str, port: int, username: str, password: str):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.identifier = nanoid.generate()
