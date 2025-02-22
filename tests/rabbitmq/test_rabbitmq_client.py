c849b8454bf48094706577e6c281ebb107bfae80
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-22 14:47:01 -0300

    refactor(tests): simplify RabbitMQClient test methods

--- tests/rabbitmq/test_rabbitmq_client.py
+++ tests/rabbitmq/test_rabbitmq_client.py
@@ -1,5 +1,4 @@
 import pytest
-
 from bababel.rabbitmq.rabbitmq_client import RabbitMQClient
 
 
@@ -19,7 +18,7 @@ def mock_conn(mocker):
 
 
 class TestRabbitMQClient:
-    def setup_method(self, ):
+    def setup_method(self):
         self.host = 'host'
         self.port = 1234
         self.username = 'username'
@@ -29,31 +28,11 @@ class TestRabbitMQClient:
     def sut(self):
         return RabbitMQClient()
 
-    def test_should_return_connection(self, sut, mock_conn):
-        # GIVEN
-        # WHEN
+    def test_connect(self, sut, mock_conn, mock_conn_params, mock_credentials):
         response = sut.connect(host=self.host, port=self.port, username=self.username, password=self.password)
-        # THEN
+
         assert response == mock_conn.return_value
-
-    def test_should_call_connection_with_correct_params(self, sut, mock_conn, mock_conn_params):
-        # GIVEN
-        # WHEN
-        sut.connect(host=self.host, port=self.port, username=self.username, password=self.password)
-        # THEN
         mock_conn.assert_called_once_with(parameters=mock_conn_params.return_value)
-
-    def test_should_call_connection_parameters_with_correct_params(self, sut, mock_credentials):
-        # GIVEN
-        # WHEN
-        sut.connect(host=self.host, port=self.port, username=self.username, password=self.password)
-        # THEN
         mock_credentials.assert_called_once_with(username=self.username, password=self.password)
-
-    def test_should_call_credentials_with_correct_params(self, sut, mock_conn_params, mock_credentials):
-        # GIVEN
-        # WHEN
-        sut.connect(host=self.host, port=self.port, username=self.username, password=self.password)
-        # THEN
         mock_conn_params.assert_called_once_with(host=self.host, port=self.port,
                                                  credentials=mock_credentials.return_value)
