48947342d4374dda3f0af78ccfa371ad67076771
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-22 18:22:35 -0300

    feat(consumer): implement IConsumer interface in Consumer class

---
+++ bababel/abstracts/consumer.py
@@ -0,0 +1,13 @@
+from abc import abstractmethod, ABC
+
+from bababel.dataclasses.queue_callback_bind import QueueCallbackBind
+
+
+class IConsumer(ABC):
+    @abstractmethod
+    def declare_bind(self, queue_callback_bind: QueueCallbackBind):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def start(self):
+        raise NotImplementedError()
