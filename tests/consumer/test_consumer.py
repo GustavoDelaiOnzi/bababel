96b4529a6bdd7f18cf9fc64c0870fdca5be4131d
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-23 18:24:17 -0300

    test(consumer): simplify tests

--- tests/consumer/test_consumer.py
+++ tests/consumer/test_consumer.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 import pytest
 
 from bababel import Consumer
@@ -5,7 +7,7 @@ from bababel import Consumer
 
 @pytest.fixture(autouse=True)
 def mock_client(mocker):
-    yield mocker.patch("bababel.consumer.consumer.RabbitMQClient")
+    yield mocker.patch("bababel.consumer.consumer.RabbitMQClient", MagicMock())
 
 
 class TestWorker:
@@ -27,36 +29,20 @@ class TestWorker:
     def mock_channel(self, mock_conn):
         yield mock_conn.return_value.establish
 
+    @pytest.fixture
+    def mock_declare(self, mock_conn):
+        yield mock_conn.return_value.queue_declare
+
+    @pytest.fixture
+    def mock_consume(self, mock_conn):
+        yield mock_conn.return_value.basic_consume
+
     def test_should_connect(self, sut, binds, mock_conn):
-        # GIVEN
         bind = binds[0]
-        # WHEN
-        sut.declare_bind(queue_callback_bind=bind)
-        # THEN
-        assert sut.connection == mock_conn.return_value
 
-    def test_should_declare_queue_with_correct_params(self, sut, binds, mock_conn):
-        # GIVEN
-        mock_declare = mock_conn.return_value.queue_declare
-        bind = binds[0]
-        # WHEN
-        sut.declare_bind(queue_callback_bind=bind)
-        # THEN
-        mock_declare.assert_called_once_with(queue=bind.queue, durable=True)
-
-    def test_should_basic_consume_with_correct_params(self, sut, binds, mock_conn):
-        # GIVEN
-        mock_basic_consume = mock_conn.return_value.basic_consume
-        bind = binds[0]
-        # WHEN
-        sut.declare_bind(queue_callback_bind=bind)
-        # THEN
-        mock_basic_consume.assert_called_once_with(queue=bind.queue, on_message_callback=bind.callback)
-
-    def test_should_return_none_on_consume(self, sut, binds):
-        # GIVEN
-        bind = binds[0]
-        # WHEN
         response = sut.declare_bind(queue_callback_bind=bind)
-        # THEN
+
+        assert sut.connection == mock_conn.return_value
+        sut.connection.queue_declare.assert_called_once_with(queue=bind.queue, durable=True)
+        sut.connection.basic_consume.assert_called_once_with(queue=bind.queue, on_message_callback=bind.callback)
         assert response is None
