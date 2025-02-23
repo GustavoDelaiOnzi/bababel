05a88040f2abbf3c78e5e737317c31ebd2abec7f
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-22 21:56:18 -0300

    feat(publisher): enhance Publisher class with publish method and refactor initialization
    - Fix BababelApp circular import, not the best solution, but it works.
    - Implementations of signature can be improved here.
    - _inspect_arguments is implemented to avoid send method being called with wrong arguments.

--- bababel/publisher/publisher.py
+++ bababel/publisher/publisher.py
@@ -1,16 +1,17 @@
-from bababel.abstracts.client import IClient
-from bababel.abstracts.publisher import IPublisher
 from bababel.bababel_app import BababelApp
 from bababel.rabbitmq.rabbitmq_client import RabbitMQClient
 
 
-class Publisher(IPublisher):
+class Publisher:
     def __init__(self, app: BababelApp):
-        self.client: IClient = RabbitMQClient()
+        self.client = RabbitMQClient()
         self.connection = self.client.connect(host=app.host,
                                               port=app.port,
                                               username=app.username,
                                               password=app.password)
+        self.app = app
 
-    def publish(self):
-        raise NotImplementedError()
+    def publish(self, task_name: str, body: str | bytes):
+        self.connection.publish(exchange=self.app.identifier,
+                                routing_key=task_name,
+                                body=body)
