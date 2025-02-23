1b8517e4ae2608512aced92e540c8235724ee6b6
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-23 17:07:03 -0300

    feat(exceptions): add BababelError base exception class

---
+++ bababel/exceptions/base_bababel_error.py
@@ -0,0 +1,7 @@
+class BababelError(Exception):
+    """Base exception class for this lib,"""
+
+    def __init__(self, message: str = None, code: str = None, details: str = None):
+        self.message = message
+        self.code = code
+        self.details = details
