631fea3e8ea4ff3ed7c0c3a43fe694364ae4154d
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-23 17:56:16 -0300

    test(publisher): add unit tests for Publisher class

---
+++ tests/publisher/test_publisher.py
@@ -0,0 +1,22 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from bababel.publisher.publisher import Publisher
+
+
+@pytest.fixture(autouse=True)
+def mock_connect(mocker):
+    yield mocker.patch('bababel.publisher.publisher.RabbitMQClient', MagicMock())
+
+
+class TestPublisher:
+    @pytest.fixture
+    def sut(self):
+        return Publisher(app=MagicMock())
+
+    def test_should_publish(self, sut, mock_connect):
+        sut.publish(task_name='xpto', body=b'xpto')
+        sut.connection.publish.assert_called_once_with(exchange=sut.app.identifier,
+                                                       routing_key='xpto',
+                                                       body=b'xpto')
