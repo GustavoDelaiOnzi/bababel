24ae044225747cdf88bbb36d5c3fe331f49442fa
GustavoOnzi <gustavodelai11@gmail.com>
2025-02-25 21:17:54 -0300

    chore: remove unused singleton

--- bababel/utils/singleton.py
+++
@@ -1,8 +0,0 @@
-class SingletonMeta(type):
-    """Metaclass that ensures subclasses are singletons, but not the base class."""
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in SingletonMeta._instances:
-            SingletonMeta._instances[cls] = super().__call__(*args, **kwargs)
-        return SingletonMeta._instances[cls]
