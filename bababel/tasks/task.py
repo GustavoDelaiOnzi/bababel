b813f23019f23be6361ff66c0cb1f63074ce5c5d
GitBlameMe <73192677+GustavoDelaiOnzi@users.noreply.github.com>
2025-02-27 22:09:18 -0300

    Revert "docs: remove docstrings and doc dir"
    This reverts commit 7882b9df20c620434b85400ae7c2996a59f60729.

--- bababel/tasks/task.py
+++ bababel/tasks/task.py
@@ -5,17 +5,52 @@ from abc import ABC, abstractmethod
 from bababel.tasks.exceptions import TaskError
 
 
-class Task(ABC):
+class Task(ABC):  # find a way for this to be sigleton or other thing that works
+    """
+    Abstract base class for defining asynchronous tasks.
+
+    This class enforces the structure for task execution by requiring subclasses
+    to implement the `run` method and define a `app` attribute.
+
+    Example:
+        Creating and executing a task:
+
+        class HelloWorld(Task)
+            def run(*args, **kwargs):
+                print('Hello World')
+
+        task = HelloWorld(app="example")
+        task.run(*args, **kwargs)
+
+        Alternatively, you can call the task instance directly:
+
+        task(*args, **kwargs)
+
+    Attributes:
+        name (str): The class name, set dynamically. It is also the identifier of the task and must be unique.
+    """
+
     def __new__(cls, *args, **kwargs):
+        """
+        Creates a new instance of a Task subclass.
+        """
         instance = super().__new__(cls)
-        cls.name = re.sub(r'(?<!^)(?=[A-Z])', '_', cls.__name__).lower()
+        cls.name = re.sub(r'(?<!^)(?=[A-Z])', '_', cls.__name__).lower()  # Converts to snake case
+        # TODO: make a utils function
+
         return instance
 
     def __init__(self, app):
+        """
+        Args:
+            app (BababelApp): The app to be assigned to the instance.
+            consumer (Consumer): The consumer that will process the tasks.
+        """
         self.app = app
 
     @abstractmethod
     def run(self, *args, **kwargs):
+        """The main process of the task."""
         raise NotImplementedError()
 
     def send(self, *args, **kwargs):
@@ -27,18 +62,22 @@ class Task(ABC):
         try:
             sig.bind(*args, **kwargs)
         except TypeError as e:
+            # Build a string for the expected parameters and their annotations
             expected_params = ', '.join(
                 f"{name}: {param.annotation.__name__}"
                 for name, param in sig.parameters.items()
             )
+            # Build a string for the received positional args
             received_args = ', '.join(
                 f"{name}: {value}"
                 for name, value in zip(sig.parameters, args)
             )
+            # Build a string for the received keyword args
             received_kwargs = ', '.join(
                 f"{key}: {value}"
                 for key, value in kwargs.items()
             )
+            # Raise an error with a clear diagnostic message
             raise TaskError(
                 "Invalid arguments: "
                 f"Expected (parameters: {expected_params}), "
---
+++ docs/docs/source/bababel.rabbitmq.rst
@@ -0,0 +1,29 @@
+bababel.rabbitmq package
+========================
+
+Submodules
+----------
+
+bababel.rabbitmq.rabbitmq\_client module
+----------------------------------------
+
+.. automodule:: bababel.rabbitmq.rabbitmq_client
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+bababel.rabbitmq.rabbitmq\_connection module
+--------------------------------------------
+
+.. automodule:: bababel.rabbitmq.rabbitmq_connection
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: bababel.rabbitmq
+   :members:
+   :show-inheritance:
+   :undoc-members:
