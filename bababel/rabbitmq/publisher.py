2c5992feda5242856dc82f0043b340f5c1c474af
GustavoOnzi <gustavodelai11@gmail.com>
2025-03-01 03:51:35 -0300

    feat(publisher): declare exchange on publish
    Ensure exchange exists and it's valid before publish.

--- bababel/rabbitmq/publisher.py
+++ bababel/rabbitmq/publisher.py
@@ -8,12 +8,12 @@ class RabbitMQPublisher(IPublisher):
     def __init__(self, connection: RabbitMQConnection, exchange: str):
         self.connection = connection
         self.exchange = exchange
-        self.__declare_rabbitmq_exchange()
 
     def publish(self, task: Task, event: dict):
+        self._declare_rabbitmq_exchange()
         self.connection.publish(exchange=self.exchange,
                                 routing_key=task.name,
                                 body=dict_to_bytes(event))
 
-    def __declare_rabbitmq_exchange(self):
+    def _declare_rabbitmq_exchange(self):
         self.connection.declare_exchange(exchange=self.exchange)
